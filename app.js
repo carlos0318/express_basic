a0accc6cd9db6614a67e95ac45d2410d85351cd0
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-04-21 20:37:10 -0500

    Agregar una nueva ruta que reciba Query Params

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ app.get('/explorersInNode', (req, res) => {
   res.send(explorer);
 })
 
+// Query Params: Recibir parámetros por la url
+// http://localhost:3000/explorers/carlos
+// req.params = {"explorerName": "carlos"}
+app.get('/explorers/:explorerName', (req, res) => {
+  res.send(req.params)
+})
+
 // con esto inicializamos esta app
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
